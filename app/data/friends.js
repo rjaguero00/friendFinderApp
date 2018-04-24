8741ce102c35f108588824bae00313d55862cfa0
Roberto aguero <rjaguero00@gmail.com>
2018-04-24 13:57:51 -0700

    90%

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,30 @@
+var friends = [
+    {
+        name: "Jimbo",
+        photo: "https://images-na.ssl-images-amazon.com/images/I/51IDNAZMKvL._SX375_BO1,204,203,200_.jpg",
+        scores: ["3", "4", "5", "2", "5", "4", "1", "3", "5", "4"]
+    },
+    {
+        name: "Shabooboo",
+        photo: "https://mtonews.com/.image/t_share/MTUzODEzODU1ODU5Nzc5MzI2/image-placeholder-title.jpg",
+        scores: ["5", "1", "5", "2", "1", "3", "5", "1", "5", "1"]
+    },
+    {
+        name: "Tay Tay",
+        photo: "https://images.rapgenius.com/7ebff18174c2a962a92f06873ca1a066.639x480x1.jpg",
+        scores: ["3", "2", "5", "1", "1", "2", "1", "1", "3", "1"]
+    },
+    {
+        name: "Allison",
+        photo: "https://images.rapgenius.com/7ebff18174c2a962a92f06873ca1a066.639x480x1.jpg",
+        scores: ["1", "5", "5", "5", "2", "5", "5", "3", "5", "3"]
+    },
+    {
+        name: "Tiffany",
+        photo: "https://getdangoodman.com/wp-content/uploads/2010/08/real-nigga-shyt8.jpg",
+        scores: ["5", "5", "5", "3", "3", "5", "5", "1", "5", "1"]
+    }
 
+]
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -13,73 +13,9 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Customers (DATA)
-// =============================================================
-var tables = [
-    {
-        "customerName": "Penis",
-        "phoneNumber": "520-123-4567",
-        "customerEmail": "penis@pubes.com",
-        "customerID": "Penis"
-    },
-    {
-        "customerName": "Balls",
-        "phoneNumber": "602-301-5251",
-        "customerEmail": "balls@nuts.com",
-        "customerID": "Balls"
-    }
-];
-
 // Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
-
-// Displays all tables
-app.get("/api/tables", function (req, res) {
-    return res.json(tables);
-});
-
-// // Displays a single character, or returns false
-// app.get("/api/characters/:character", function(req, res) {
-//   var chosen = req.params.character;
-
-//   console.log(chosen);
-
-//   for (var i = 0; i < characters.length; i++) {
-//     if (chosen === characters[i].routeName) {
-//       return res.json(characters[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
-
-// Create New Table Reservations - takes in JSON input
-app.post("/api/tables", function (req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body-parser middleware
-    var newTable = req.body;
-
-    // Using a RegEx Pattern to remove spaces from newtable
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    //newTable.routeName = newTable.name.replace(/\s+/g, "").toLowerCase();
-
-    console.log(newTable);
-
-
-    tables.push(newTable);
-    console.log(tables);
-
-    res.json(newTable);
-});
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // Starts the server to begin listening
 // =============================================================
